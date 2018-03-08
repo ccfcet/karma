48229ecdd035853093b14c47c9799dca142c7a61
aashan007 <sarathchandran1995@gmail.com>
2018-03-08 20:42:12 +0000

    event information slug

---
+++ src/models/Events/event_information_slugs.js
@@ -0,0 +1,20 @@
+'use strict'
+
+module.exports = function (sequelize, DataTypes) {
+  var eventInformationSlugs = sequelize.define('event_information_slugs', {
+    id: {
+      type: DataTypes.INTEGER(),
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    slug_name: {
+      type: DataTypes.VARCHAR(45),
+      allowNull: false
+    }
+  })
+
+
+
+  return FacultyAcademicEnrolmentActivity
+}
