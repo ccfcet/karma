47fb08c4b4ad4871ef10ad2353fec498d78e78cc
aashan007 <sarathchandran1995@gmail.com>
2018-03-08 20:03:09 +0000

    added models roles

--- src/models/Media/media_role_people_association.js
+++ src/models/Media/media_role_people_association.js
@@ -1,24 +1,38 @@
 'use strict'
 
 module.exports = function (sequelize, DataTypes) {
-  var Entities = sequelize.define('entities', {
+  var media_role_people_association = sequelize.define('media_role_people_association', {
     id: {
       type: DataTypes.INTEGER(),
       primaryKey: true,
       autoIncrement: true
     },
-    entity_name: {
+    media_id: {
       type: DataTypes.STRING(255),
       allowNull: false,
       unique: true
     },
-    entity_type_id: {
+    role_id: {
       type: DataTypes.INTEGER(),
       allowNull: false
+    },
+    people_id: {
+      type: DataTypes.INTEGER(),
+      allowNull: false
+    },
+    created_at:{
+      type: DataTypes.TIME(),
+      allowNull: false
+    },
+    updated_at:{
+      type: DataTypes.TIME(),
+      allowNull: false
     }
+
+
   })
 
-  Entities.associate = function (models) {
+  media_role_people_association.associate = function (models) {
     models.Entities.entities.belongsTo(models.Entities.entity_types, {
       onDelete: 'CASCADE',
       foreignKey: {
