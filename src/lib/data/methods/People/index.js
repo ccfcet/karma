23854a20b8f5b10bd705dce8fe890036a28d43e4
santhisenan <santhisenan@outlook.com>
2018-06-05 06:40:19 +0530

    added routes for people layer

--- src/lib/data/methods/People/index.js
+++ src/lib/data/methods/People/index.js
@@ -2,6 +2,7 @@ var Promise = require('bluebird')
 
 var models = require('_/data/models')
 var peopleMethods = {}
+
 peopleMethods.addPeople = (info) => {
   return new Promise((resolve, reject) => {
     models.People.people.create(info)
@@ -15,4 +16,61 @@ peopleMethods.addPeople = (info) => {
   })
 }
 
+peopleMethods.findPeopleById = (peopleId) => {
+  return new Promise((resolve, reject) => {
+    models.People.people.findById(peopleId)
+      .then((person) =>
+        resolve(person)
+      )
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+peopleMethods.getInforamationUsingSlug = (peopleId, informationSlug) => {
+  return new Promise((resolve, reject) => {
+    models.People.people.findById(peopleId)
+      .then((person) => {
+        models.People.people_information_slugs.findOne(
+          {
+            where: { slug_name: informationSlug }
+          })
+          .then((slug) => {
+            models.People.people_information.findOne({
+              where: {
+                id: peopleId,
+                slug_name: slug.slug_name
+              }
+            })
+              .then((peopleInfomation) => {
+                resolve(peopleInfomation)
+              })
+              .catch((err) => {
+                reject(err)
+              })
+          })
+          .catch((err) => {
+            reject(err)
+          })
+      })
+      .catch((err) => {
+        reject(err)
+      })
+  })
+}
+
+peopleMethods.insertSlug = (slugName, peopleId) => {
+  return new Promise((resolve, reject) => {
+    models.People.people_information_slugs.findOrCreate({
+      where: { slug_name: slugName }
+    })
+    .then( (arr) => 
+    .spread((slug, created) => {
+      if(!created) {
+
+      }
+    })
+  })
+}
 module.exports = peopleMethods
