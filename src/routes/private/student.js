6d1dcabaddb30f343f9178e8e91394f01a463307
Bilal <bilaljaleel123@gmail.com>
2018-06-08 11:16:10 +0530

    lint error fixed

---
+++ src/routes/private/student.js
@@ -0,0 +1,9 @@
+// var express = require('express')
+// var router = express.Router()
+// var methods = require('_/data/methods')
+
+// router.post('/', function (req, res) {
+//   methods.student
+// })
+
+// module.exports = router
