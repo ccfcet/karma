c084f73fa57431f185bed52901cc4439a3ed9207
Bilal <bilaljaleel123@gmail.com>
2018-06-08 16:25:33 +0530

    "Student Course Grading Standard"

--- src/lib/data/methods/student/index.js
+++ src/lib/data/methods/student/index.js
@@ -1,5 +1,5 @@
 var studentMethods = {}
 
 studentMethods.CourseGrades = require('./student_course_grades')
-
+studentMethods.CourseGradingStandard = require('./student_course_grading_standards')
 module.exports = studentMethods
--- src/routes/private/student/index.js
+++ src/routes/private/student/index.js
@@ -2,5 +2,5 @@ var express = require('express')
 var router = express.Router()
 
 router.use('/student_course_grades', require('./student_course_grades'))
-
+router.use('/student_course_grading_standards', require('./student_course_grading_standards'))
 module.exports = router
