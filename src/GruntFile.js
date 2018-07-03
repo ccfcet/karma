106ba6ecf7979936f83510e8990299efbcd9e69a
Santhisenan <santhisenan@outlook.com>
2018-07-03 09:52:40 +0530

    added grunt and apidoc

---
+++ src/GruntFile.js
@@ -0,0 +1,13 @@
+module.exports = (grunt) => {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        apidoc: {
+            myapp: {
+              src: "./routes",
+              dest: "../docs/apidoc/"
+            }
+          }
+    })
+
+    grunt.loadNpmTasks('grunt-apidoc');
+}
