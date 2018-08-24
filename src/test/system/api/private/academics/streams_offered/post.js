c3239e95cb8742b6363fc2739673a1ccaf4c5918
Chithra98 <sprlic16@gmail.com>
2018-08-24 23:27:36 +0530

    testing

---
+++ src/test/system/api/private/academics/streams_offered/post.js
@@ -0,0 +1,144 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const chaiExclude = require('chai-exclude');
+
+chai.use(chaiExclude);
+const app = require('../../../../../../bin/www');
+const methods = require('../../../../../../lib/data/methods');
+
+
+process.nextTick(() => {
+  app.callback = run;
+});
+
+chai.use(chaiHttp);
+const { expect } = chai;
+
+const streamType = [];
+const newEntity = [];
+const newEntityType = [];
+const streamsOffered = []
+const newStreams = [];
+
+describe('Post stream types - POST', () => {
+  beforeEach((done) => {
+    methods.Academics.streamTypesMethods.deleteAllStreamTypes().then(() => {
+        console.log('deleted streamtypes');
+        methods.Entities.entityTypeMethods.deleteAllEntityTypes().then(() => {
+          console.log('deleted entitytypes');
+          methods.Entities.entityMethods.deleteAllEntity().then(() => {
+            console.log('deleted entities');
+  
+            methods.Academics.streamsOfferedMethods.deleteAllStreamsOffered()
+              .then(() => {
+               
+                const classes = {
+                    stream_type_long: '5',
+                    stream_type_short: '5',
+                    start_date: '2018-07-25',
+                    end_date: '2018-07-29',
+                  };
+              
+                  methods.Academics.streamTypesMethods.addStreamType(classes)
+                    .then((model) => {
+                      streamType.push(model.dataValues);
+                      const entityTypes = {
+                        entity_type: 'people',
+                        entity_type_slug: 'about',
+              
+                      };
+                      console.log(streamType);
+              
+                      methods.Entities.entityTypeMethods.addEntityType(entityTypes)
+                        .then((entityType) => {
+                          console.log('entered entity type');
+                          newEntityType.push(entityType.dataValues);
+                          const entity = {
+                            entity_name: 'cse',
+                            entity_slug: 'about',
+                            entity_type_id: newEntityType[0].id,
+                          };
+                          methods.Entities.entityMethods.addEntity(entity)
+                            .then((entities) => {
+                              newEntity.push(entities.dataValues);
+                              console.log(streamType);
+                                done();
+
+                            })
+                            .catch((err) => {
+                              console.log(err);
+                            });
+                        })
+                        .catch((err) => {
+              
+                        });
+                    })
+                    .catch(err => console.log(err));
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
+  it('POST /public/academics/stream_types/', (done) => {
+    const newStream = {
+              
+        stream_type_id: streamType[0].id,
+        stream_name: 'btech',
+        department_id: newEntity[0].id,
+        valid_start_date: '2018-07-25',
+        valid_end_date: '2018-07-25',
+      };
+
+    chai.request(app)
+      .post('/public/academics/streams_offered/')
+      .send(newStream)
+      .end((err, result) => {
+        expect(result).to.have.status(200);
+        expect(result.body).to.be.a('object');
+
+        done();
+      });
+  });
+  afterEach((done) => {
+    methods.Academics.streamTypesMethods.deleteAllStreamTypes().then(() => {
+      console.log('deleted streamtypes');
+      methods.Entities.entityTypeMethods.deleteAllEntityTypes().then(() => {
+        console.log('deleted entitytypes');
+        methods.Entities.entityMethods.deleteAllEntity().then(() => {
+          console.log('deleted entities');
+
+          methods.Academics.streamsOfferedMethods.deleteAllStreamsOffered()
+            .then(() => {
+              console.log('deleted');
+              done();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+        .catch((err) => {
+          console.log(err);
+        });
+    })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+});
