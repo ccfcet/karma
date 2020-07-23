02efe848e3e4821c888308b10590c60182c51fb1
Mohammed Rabeeh <mohammadrabeeh@gmail.com>
2020-07-23 18:52:20 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const app = express();
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(helmet());
+
+//Routes
+app.get('/secret-page', (req, res) => {
+  res.status(200).send("You found the secret page. Whaaaa?");
+});
+
+app.get('/', (req, res) => {
+  res.status(200).send("It is what it is.");
+});
+
+// Server start-up configuration
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening for requests on http://localhost/${port}`);
+});
